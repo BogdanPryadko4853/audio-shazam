02630d1b24bf3c9e2a78f643e9833e5bcfcacddd
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-06-30 17:49:12 +0300

    config: setup kafka

---
+++ audio-fingerprint-service/src/main/java/com/audio/audiofingerprintservice/config/KafkaConfig.java
@@ -0,0 +1,36 @@
+package com.audio.audiofingerprintservice.config;
+
+import com.audio.audiofingerprintservice.model.FingerprintProcessedEvent;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ProducerFactory<String, FingerprintProcessedEvent> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, FingerprintProcessedEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
