bffbe6f61a91e3936fe2f4ed6e3adc08a5a348a9
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-07-21 10:28:39 +0300

    refactor: add class corsconfig

--- gateway-service/src/main/java/com/audio/gatewayservice/config/CorsConfig.java
+++ gateway-service/src/main/java/com/audio/gatewayservice/config/CorsConfig.java
@@ -1,16 +1,24 @@
 package com.audio.gatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.server.ServerWebExchange;
 
-@Bean
-public CorsWebFilter corsFilter() {
-    return new CorsWebFilter(exchange -> {
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(this::getCorsConfiguration);
+    }
+
+    private CorsConfiguration getCorsConfiguration(ServerWebExchange exchange) {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         return config;
-    });
+    }
 }
